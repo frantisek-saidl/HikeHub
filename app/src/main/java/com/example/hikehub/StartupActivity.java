42d85b857597c5d74764fe3d57872d3d29f7db7a
Fanda Saidl <187737935+frantisek-saidl@users.noreply.github.com>
2025-03-03 10:27:02 +0100

    Refactor login and sign-up layouts; add startup activity and button color selector

--- app/src/main/java/com/example/hikehub/StartupActivity.java
+++ app/src/main/java/com/example/hikehub/StartupActivity.java
@@ -8,24 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        // Set padding based on system bar insets
+        setContentView(R.layout.activity_startup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Initialize buttons and set their onClick listeners
-        Utils.setupButton(this, R.id.buttonMap, MapActivity.class);
-        Utils.setupButton(this, R.id.buttonProfile, ProfileActivity.class);
-        Utils.setupButton(this, R.id.buttonNewHike, NewHikeActivity.class);
+        Utils.setupButton(this, R.id.buttonLogin, LoginActivity.class);
+        Utils.setupButton(this, R.id.buttonSignUp, SignUpActivity.class);
     }
 }
